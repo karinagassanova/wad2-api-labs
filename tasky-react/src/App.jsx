776d9d01bdd3e6f13d74aae88032a53778d26975
karinegassanova <20102374@mail.wit.ie>
2025-12-02 14:09:18 +0000

    Added new pages and routing to Tasky React app

--- tasky-react/src/App.jsx
+++ tasky-react/src/App.jsx
@@ -1,93 +1,42 @@
-import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router";
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import LoginPage from "./pages/loginPage";
+import SignupPage from "./pages/signupPage";
+import TasksPage from "./pages/tasksPage";
+import StartPage from "./pages/startPage";
+import ProfilePage from "./pages/profilePage";
 import './App.css';
-import Task from './components/Task';
-import AddTaskForm from './components/Form';
-import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  const [taskState, setTaskState] = useState({
-    tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", priority: "High", done: false },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "Medium", done: false },
-      { id: 3, title: "Tidy up", deadline: "Today", priority: "Low", done: false }
-    ]
-  });
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000,
+      refetchOnWindowFocus: false
+    },
+  },
+});
 
-  const [formState, setFormState] = useState({
-    title: "",
-    description: "",
-    deadline: "",
-    priority: "" 
-  });
-
-  const doneHandler = (taskIndex) => {
-    const tasks = [...taskState.tasks];
-    tasks[taskIndex].done = !tasks[taskIndex].done;
-    setTaskState({ tasks });
-    console.log(`${taskIndex} ${tasks[taskIndex].done}`);
-  };
-
-  const deleteHandler = (taskIndex) => {
-    const tasks = [...taskState.tasks];
-    tasks.splice(taskIndex, 1);
-    setTaskState({ tasks });
-  };
-
-  const formChangeHandler = (event) => {
-    let form = { ...formState };
-
-    switch (event.target.name) {
-      case "title":
-        form.title = event.target.value;
-        break;
-      case "description":
-        form.description = event.target.value;
-        break;
-      case "deadline":
-        form.deadline = event.target.value;
-        break;
-      case "priority":
-        form.priority = event.target.value;
-        break;
-      default:
-        form = formState;
-    }
-
-    setFormState(form);
-  };
-
-  const formSubmitHandler = (event) => {
-    event.preventDefault();
-
-    const tasks = [...taskState.tasks];
-    const form = { ...formState };
-
-    form.id = uuidv4();
-    form.done = false; 
-
-    tasks.push(form);
-    setTaskState({ tasks });
-    setFormState({ title: "", description: "", deadline: "", priority: "" }); 
-  };
-
+const App = () => {
   return (
-    <div className="container">
-      <h1>Tasky</h1>
-      {taskState.tasks.map((task, index) => (                        
-        <Task 
-          key={task.id}
-          title={task.title}
-          description={task.description}
-          deadline={task.deadline}
-          priority={task.priority}  
-          markDone={() => doneHandler(index)}
-          done={task.done}
-          deleteTask={() => deleteHandler(index)}
-        />
-      ))} 
-      <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <div className="container">
+          <h1>Tasky</h1>
+          <Routes>
+            <Route path="/" element={< StartPage />} />
+            <Route path="/login" element={< LoginPage />} />
+            <Route path="/signup" element={< SignupPage />} />
+            <Route path="/tasks" element={< TasksPage />} />
+            <Route path="/profile" element={< ProfilePage />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
